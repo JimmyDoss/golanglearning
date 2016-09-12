98701a721b5f839af3cf986e2990de75385d7aa3
complexsion <jamesdoss08@gmail.com>
2016-09-12 04:47:46 -0700

    first couple of programs of go - seems fun

---
+++ 02_numeral-systems/03_hexadecimal/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("%d - %b - %x \n", 42, 42, 42)
+	// fmt.Println("%d - %b - %#x \n", 42, 42, 42)
+	// fmt.Println("%d - %b - %#X \n", 42, 42, 42)
+
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+}
